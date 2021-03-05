4fc5776cbdfe9727c95bb3ab14171eda2b52d07c
Enigmatlc <30873095+Enigmatlc@users.noreply.github.com>
2021-03-05 16:13:57 -0500

    changed application icon

--- Project 3/src/payrollsystem/Main.java
+++ Project 3/src/payrollsystem/Main.java
@@ -6,13 +6,14 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+import javafx.scene.image.Image;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
     	BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("View.fxml"));
-        //primaryStage.setTitle("Hello World");
+        primaryStage.getIcons().add(new Image("https://i.ibb.co/DGTHRP1/cat.png"));
         primaryStage.setScene(new Scene(root, 600, 600));
         primaryStage.show();
     }
